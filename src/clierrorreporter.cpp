df7ae1f0eb5058787441d658ed4c8c6bb6f0ce2c
Francesco Nwokeka <francesco.nwokeka@gmail.com>
2012-01-10 12:39:19 +0100

    ADD error reporter class
    has a static method to call for printing errors to cli

---
+++ src/clierrorreporter.cpp
@@ -0,0 +1,45 @@
+/*
+ * srvAgatha
+ *
+ * This file is part of srvAgatha
+ * Copyright (C) 2012 Francesco Nwokeka <francesco.nwokeka@gmail.com>
+ *
+ */
+
+#include "clierrorreporter.h"
+
+#include <QtCore/QDebug>
+
+#define YELLOW "\e[1;33m"
+#define RED "\e[0;31m"
+#define BOLD_RED "\e[1;31m"
+#define COLOR_END "\e[0m"
+
+void CliErrorReporter::printError(CliErrorReporter::ErrorType type, CliErrorReporter::ErrorLevel lvl, const QString& errMsg)
+{
+    QString cliMsg;
+
+    // set colour
+    if (lvl == WARNING) {
+        cliMsg.append(YELLOW);
+    } else if (lvl == ERROR) {
+        cliMsg.append(RED);
+    } else if (lvl == CRITICAL) {
+        cliMsg.append(BOLD_RED);
+    }
+
+    if (type == APPLICATION) {
+        cliMsg.append("[AgathaServer:Application] : ");
+    } else if (type == DATABASE) {
+        cliMsg.append("[AgathaServer:Database] : ");
+    }
+
+    // add error message
+    cliMsg.append(errMsg);
+
+    // close colour
+    cliMsg.append(COLOR_END);
+
+    // print
+    qWarning() << cliMsg;
+}
---
+++ src/clierrorreporter.h
@@ -0,0 +1,43 @@
+/*
+ * srvAgatha
+ *
+ * This file is part of srvAgatha
+ * Copyright (C) 2012 Francesco Nwokeka <francesco.nwokeka@gmail.com>
+ *
+ */
+
+#ifndef CLIERRORREPORTER_H
+#define CLIERRORREPORTER_H
+
+#include <QtCore/QString>
+
+/**
+ * Class used to print error messages on the console during runtime
+ * @author Francesco Nwokeka
+ */
+
+class CliErrorReporter
+{
+public:
+    enum ErrorType {
+        APPLICATION,    /** application related error */
+        DATABASE        /** database related error */
+    };
+
+    enum ErrorLevel {
+        WARNING,        /** simple warning (yellow) */
+        ERROR,          /** an error that doesn't compromise program stability (red) */
+        CRITICAL        /** system crash/stability compromised. (bold red) */
+    };
+
+    /** print error to screen. According to the error lvl, prints will have different colours
+     * @param type error type
+     * @param lvl error level
+     * @param errMsg error message to display
+     */
+    static void printError(ErrorType type, ErrorLevel lvl, const QString &errMsg);
+};
+
+
+
+#endif  // CLIERRORREPORTER_H
