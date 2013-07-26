9b6743866d084a338a8fa945dc450575d3516b67
Francesco Nwokeka <francesco.nwokeka@gmail.com>
2013-07-26 21:16:45 +0200

    removed QJson dependency and add missing files. We'll be using Qt5's json

---
+++ src/enums.h
@@ -0,0 +1,15 @@
+/*
+ * enums.h
+ *
+ * This file is part of Agatha
+ * Copyright (C) 2012-2013 Francesco Nwokeka <francesco.nwokeka@gmail.com>
+ * Author Francesco Nwokeka
+ */
+
+/**
+ * @brief this file holds the ENUMS used in the server application by more than one class
+ **/
+
+enum GAMETYPE {
+    URBAN_TERROR_4_1_X
+};
